a5e748b60e97c17560afab3652d540219e5b8b06
DevRaj5677 <developeraj5677@gmail.com>
2025-06-17 08:13:34 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:notes_app/pages/home_page.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(
-    DevicePreview(
-      builder: (context) => MyApp(), // Wrap your app
-    ),
-  );
+  runApp(MyApp()),
 }
 
 class MyApp extends StatelessWidget {
